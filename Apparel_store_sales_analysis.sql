b3db7ef73b00f20179c9aaf4fa8c595b3e57810d
Sudhir Patil <152376879+Sudhirr1122@users.noreply.github.com>
2024-03-14 17:16:52 +0530

    Update Apparel_store_sales_analysis.sql

--- Apparel_store_sales_analysis.sql
+++ Apparel_store_sales_analysis.sql
@@ -21,116 +21,122 @@ LIMIT 5;
 
 2. Fetch the top 4 Categories with the highest sales
 
-select category,sum("Purchase Amount (USD)")sales
-from customer_details
-Group by 1
+SELECT category, SUM("Purchase Amount (USD)") AS sales
+FROM customer_details
+GROUP BY 1
 LIMIT 4;
+
 
 
 3. Fetch the top 4 Categories with the highest sales
 
-select category,sum("Purchase Amount (USD)")sales
-from customer_details
-Group by 1
+SELECT category, SUM("Purchase Amount (USD)") AS sales
+FROM customer_details
+GROUP BY 1
 LIMIT 4;
+
 
 4. Fetch the percentage of top 5 Item Purchased.
 
-select "Item Purchased",Round (COUNT (*) * 100.0 / (select COUNT (*) from customer_details),2)as total_percentage
-from 
-    customer_details
-group by 
-    "Item Purchased" 
-ORDER by
-    total_percentage desc
+SELECT "Item Purchased", ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details), 2) AS total_percentage
+FROM customer_details
+GROUP BY "Item Purchased" 
+ORDER BY total_percentage DESC
 LIMIT 5;
+
 
 5. Fetch the top 5 locations where most orders come from and tell the percentage of orders
 from those locations.
 
-select Gender, COUNT (*) * 100.0 / (select COUNT (*) from customer_details)as total_percentage
-from customer_details
-group by Gender;
+SELECT Gender, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details) AS total_percentage
+FROM customer_details
+GROUP BY Gender;
+
 
 
 6. Based on the Size column, tell the percentage of order contribution for each Size.
-select "Size",Round (COUNT (*) * 100.0 / (select COUNT (*) from customer_details),2)as total_percentage
-from 
-    customer_details
-group by 
-    1 
-ORDER by
-    total_percentage desc;
+
+SELECT "Size", ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details), 2) AS total_percentage
+FROM customer_details
+GROUP BY 1 
+ORDER BY total_percentage DESC;
+
 
 
 7. Fetch the top 4 colors bought by the customers.
 
-select "Color",Round (COUNT (*) * 100.0 / (select COUNT (*) from customer_details),2)as total_percentage
-from 
-    customer_details
-group by 
-    1 
-ORDER by
-    total_percentage DESC
+SELECT "Color", ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details), 2) AS total_percentage
+FROM customer_details
+GROUP BY 1 
+ORDER BY total_percentage DESC
 LIMIT 4;
+
 
 8. Fetch the Season which has the highest and second highest orders
 
-select Season,count(*)total
-from customer_details
-group by 1
-order by 2 DESC
+SELECT Season, COUNT(*) AS total
+FROM customer_details
+GROUP BY 1
+ORDER BY 2 DESC
 LIMIT 2;
+
 
 9. Fetch the rating which was given maximum times by the customer.
 
-select Gender, COUNT (*) * 100.0 / (select COUNT (*) from customer_details)as total_percentage
-from customer_details
-group by Gender;
+SELECT Gender, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details) AS total_percentage
+FROM customer_details
+GROUP BY Gender;
+
+
 
 
 10. Which type of shipping has the highest amount of buyers?
 
-select Gender, COUNT (*) * 100.0 / (select COUNT (*) from customer_details)as total_percentage
-from customer_details
-group by Gender;
+SELECT Gender, COUNT(*) * 100.0 / (SELECT COUNT(*) FROM customer_details) AS total_percentage
+FROM customer_details
+GROUP BY Gender;
+
 
 
 11. How many buyers have not used Promo code?
 
-select count("Promo Code Used")total
-from customer_details
-where "Promo Code Used"= NO;
+SELECT COUNT("Promo Code Used") AS total
+FROM customer_details
+WHERE "Promo Code Used" = 'NO' or '';
+
 
 12. How many buyers have purchased an item before?
 
-select count("Promo Code Used")total
-from customer_details
-where "Promo Code Used"= NO;
+SELECT COUNT("Promo Code Used") AS total
+FROM customer_details
+WHERE "Promo Code Used" = 'NO';
+
+
 
 13. What are the top 3 payment method of the buyers?
 
-select "Payment Method",count("Payment Method") as total
-from customer_details
-group by 1
-order by 2 desc
-limit 3;
+SELECT "Payment Method", COUNT("Payment Method") AS total
+FROM customer_details
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 3;
+
 
 14. Which category has the most purchased amount?
 
-select Category, sum("Purchase Amount (USD)") total
-from customer_details
-group by 1
-order by 2 desc
-limit 1;
+SELECT Category, SUM("Purchase Amount (USD)") AS total
+FROM customer_details
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
 
 15. Which gender has the most ratings?
 
-select Gender, sum("Review Rating") as total_ratings
-from customer_details
-group by 1
-order by 2 desc
-limit 1
+SELECT Gender, SUM("Review Rating") AS total_ratings
+FROM customer_details
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
 
 
 
